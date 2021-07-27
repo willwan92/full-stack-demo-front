e968982b750fb7d57443654f6f9a78a1cf2f6fd8
willwan92 <wilson3cy@163.com>
2021-07-27 17:16:18 +0800

    更新axios插件，在请求头中添加token

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,26 +3,30 @@ import axios from 'axios'
 
 // 负责axios的各种配置，前缀，token管理，路由跳转
 
-let service = axios.create({
-  timeout:5000,
-  baseURL:'/api'
+const service = axios.create({
+  timeout: 5000,
+  baseURL: '/api'
 })
 
 // @ 拦截器 管理token
 service.interceptors.request.use(
-  config=>{
+  config => {
     // 请求拦截器，后续的token管理都在这
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.common.Authorization = 'Bearer ' + token
+    }
     return config
   },
-  err=>{
+  err => {
     return Promise.reject(err)
   }
 )
 // 响应拦截器
 service.interceptors.response.use(
-  async response=>{
+  response => {
     // header config这里处理就可以了，应用层只需要数据data
-    let {data} = response
+    const { data } = response
     // if(dat)
     return data
 
